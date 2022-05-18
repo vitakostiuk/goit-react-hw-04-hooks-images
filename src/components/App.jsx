7fe0ef7abe42cd6c7a1b0efc4e861b266d616bc1
Vita Kostiuk <vitagrebennik@gmail.com>
2022-05-18 14:10:01 +0300

    task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-// import React, { Component } from 'react';
 import 'react-notifications/lib/notifications.css';
 import { BallTriangle } from 'react-loader-spinner';
 import {
@@ -24,6 +23,7 @@ export const App = () => {
   const [hiddenBtn, setHiddenBtn] = useState(true);
   const [error, setError] = useState(null);
 
+  //first render
   useEffect(() => {
     setImages(null);
     setHiddenBtn(true);
@@ -31,7 +31,7 @@ export const App = () => {
     const getImages = async () => {
       try {
         setLoading(true);
-        let images = await fetchImages(imgName, page);
+        let images = await fetchImages(imgName);
         images = images.hits;
         if (images.length === 0) {
           NotificationManager.warning(`There are not images with such name.`);
@@ -46,7 +46,31 @@ export const App = () => {
       }
     };
     if (imgName) getImages();
-  }, [imgName, page]);
+  }, [imgName]);
+
+  // pagination(load more)
+  useEffect(() => {
+    setHiddenBtn(true);
+
+    const getImages = async () => {
+      try {
+        setLoading(true);
+        let images = await fetchImages(page);
+        images = images.hits;
+        if (images.length === 0) {
+          NotificationManager.warning(`There are not images with such name.`);
+          return setImages(null);
+        }
+        setImages(prevImages => [...prevImages, ...mapper(images)]);
+        setHiddenBtn(false);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (page !== 1) getImages();
+  }, [page]);
 
   const incrementPage = () => {
     setPage(prevPage => prevPage + 1);
@@ -98,275 +122,3 @@ export const App = () => {
     </>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     imgName: '',
-//     images: null,
-//     showModal: false,
-//     largeImage: null,
-//     page: 1,
-//     loading: false,
-//     hiddenBtn: true,
-//     error: null,
-//   };
-
-//   componentDidUpdate = (prevProps, prevState) => {
-//     const { imgName, page } = this.state;
-//     const prevName = prevState.imgName;
-//     const currentName = imgName;
-//     const prevPage = prevState.page;
-//     const currentPage = page;
-
-//     if (prevName !== currentName) {
-//       this.setState({ loading: true, hiddenBtn: true, images: null });
-//       this.getImages(prevName, currentName, prevPage, currentPage);
-//     }
-
-//     if (prevName === currentName && prevPage !== currentPage) {
-//       this.setState({ loading: true, hiddenBtn: true });
-//       this.getImages(prevName, currentName, prevPage, currentPage);
-//     }
-//   };
-
-//   getImages = (prevName, currentName, prevPage, currentPage) => {
-//     return fetchImages(currentName, currentPage)
-//       .then(images => images.hits)
-//       .then(images => {
-//         if (images.length === 0) {
-//           NotificationManager.warning(`There are not images with such name.`);
-//           return this.setState({
-//             images: null,
-//           });
-//         }
-//         if (prevName !== currentName) {
-//           this.setStateForFirstRequest(images);
-//         }
-//         if (prevName === currentName && prevPage !== currentPage) {
-//           this.setStateForPagination(images);
-//         }
-//       })
-//       .catch(error => this.setState({ error }))
-//       .finally(() => this.setState({ loading: false }));
-//   };
-
-//   setStateForFirstRequest = data => {
-//     return this.setState({
-//       images: mapper(data),
-//       hiddenBtn: false,
-//     });
-//   };
-
-//   setStateForPagination = data => {
-//     return this.setState(prevState => ({
-//       images: [...prevState.images, ...mapper(data)],
-//       hiddenBtn: false,
-//     }));
-//   };
-
-//   incrementPage = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-//   handleSearchbarSubmit = imgName => {
-//     this.setState({ imgName });
-//     this.setState({ page: 1 });
-//     this.setState({ loading: false });
-//   };
-
-//   toogleModal = () => {
-//     this.setState(({ showModal }) => ({ showModal: !showModal }));
-//   };
-
-//   openImage = largeImage => {
-//     this.setState(({ showModal }) => ({ showModal: !showModal, largeImage }));
-//   };
-
-//   render() {
-//     const {
-//       images,
-//       showModal,
-//       largeImage,
-//       imgName,
-//       loading,
-//       hiddenBtn,
-//       error,
-//     } = this.state;
-
-//     return (
-//       <>
-//         <Searchbar onSubmit={this.handleSearchbarSubmit} />
-//         {error && <div>{error.message}</div>}
-//         <ImageGallery
-//           images={images}
-//           imgName={imgName}
-//           openLargeImage={this.openImage}
-//         />
-//         {loading && (
-//           <div className={s.Loader}>
-//             <BallTriangle
-//               height="50"
-//               width="50"
-//               color="#49b6ff"
-//               ariaLabel="loading"
-//             />
-//           </div>
-//         )}
-//         {!hiddenBtn && (
-//           <Button onClick={this.incrementPage} aria-label="Load more" />
-//         )}
-//         {showModal && (
-//           <Modal
-//             onClose={this.toogleModal}
-//             largeImage={largeImage}
-//             imgName={imgName}
-//           />
-//         )}
-//         <NotificationContainer />
-//       </>
-//     );
-//   }
-// }
-
-//---Варіант зі стейт-машиною і async-await----
-/*
-import React, { Component } from 'react';
-import 'react-notifications/lib/notifications.css';
-import { BallTriangle } from 'react-loader-spinner';
-import {
-  NotificationContainer,
-  NotificationManager,
-} from 'react-notifications';
-import { Searchbar } from './Searchbar';
-import { ImageGallery } from './ImageGallery';
-import { fetchImages } from '../services/pixabay-api';
-import { Modal } from './Modal';
-import { Button } from './Button';
-import s from './App.module.css';
-
-export class App extends Component {
-  state = {
-    imgName: '',
-    images: null,
-    showModal: false,
-    largeImage: null,
-    page: 1,
-    hiddenBtn: true,
-    error: null,
-    status: 'idle',
-  };
-
-  componentDidUpdate = async (prevProps, prevState) => {
-    const { imgName, page } = this.state;
-    const prevName = prevState.imgName;
-    const currentName = imgName;
-    const prevPage = prevState.page;
-    const currentPage = page;
-
-    if (prevName !== currentName) {
-      this.setState({ status: 'pending' });
-      try {
-        let images = await fetchImages(currentName, page);
-        images = images.hits;
-        if (images.length === 0) {
-          NotificationManager.warning(`There are not images with such name.`);
-          return this.setState({
-            status: 'idle',
-          });
-        }
-        this.setState({
-          images,
-          status: 'resolved',
-        });
-      } catch (error) {
-        this.setState({ error, status: 'error' });
-      }
-    }
-
-    if (prevName === currentName && prevPage !== currentPage) {
-      this.setState({ status: 'pending' });
-
-      try {
-        let images = await fetchImages(currentName, page);
-        images = images.hits;
-        return this.setState(prevState => ({
-          images: [...prevState.images, ...images],
-          status: 'resolved',
-        }));
-      } catch (error) {
-        this.setState({ error, status: 'error' });
-      }
-    }
-  };
-
-  incrementPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  handleSearchbarSubmit = imgName => {
-    this.setState({ imgName, status: 'pending' });
-    this.setState({ page: 1 });
-  };
-
-  toogleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  openImage = largeImage => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-      largeImage,
-    }));
-  };
-
-  render() {
-    const { images, showModal, largeImage, imgName, error, status } =
-      this.state;
-
-    return (
-      <>
-        {(status === 'idle' ||
-          status === 'resolved' ||
-          status === 'pending') && (
-          <Searchbar onSubmit={this.handleSearchbarSubmit} />
-        )}
-        {status === 'error' && <div>{error.message}</div>}
-        {status === 'resolved' && (
-          <ImageGallery
-            images={images}
-            imgName={imgName}
-            openLargeImage={this.openImage}
-          />
-        )}
-        {status === 'resolved' && (
-          <Button onClick={this.incrementPage} aria-label="Load more">
-            Load more
-          </Button>
-        )}
-        {status === 'pending' && (
-          <div className={s.Loader}>
-            <BallTriangle
-              height="50"
-              width="50"
-              color="#49b6ff"
-              ariaLabel="loading"
-            />
-          </div>
-        )}
-        {showModal && (
-          <Modal onClose={this.toogleModal}>
-            <img src={largeImage} alt={imgName} width="800" />
-          </Modal>
-        )}
-        <NotificationContainer />
-      </>
-    );
-  }
-}
-*/
--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,26 +1,3 @@
-// export const fetchImages = (name, page) => {
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const queryParams = new URLSearchParams({
-//     key: '25731511-e5f7726e83d52bf5fe5f97cfd',
-//     q: name,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     page,
-//     per_page: 12,
-//   });
-
-//   return fetch(`${BASE_URL}?${queryParams}`).then(res => {
-//     if (res.ok) {
-//       return res.json();
-//     }
-//     return Promise.reject(
-//       new Error(`There are not images with such name ${name}`)
-//     );
-//   });
-// };
-
-//---Варіант з async-await----
-
 export const fetchImages = async (name, page) => {
   const BASE_URL = 'https://pixabay.com/api/';
   const queryParams = new URLSearchParams({
